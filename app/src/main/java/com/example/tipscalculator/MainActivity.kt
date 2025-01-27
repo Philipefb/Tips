f7ab4ebd56ee010ad71b29c818364727815c4e86
Philipe <philipe.bitenca@hotmail.com>
2025-01-27 10:40:18 -0300

    identação

--- app/src/main/java/com/example/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -28,31 +28,23 @@ class MainActivity : AppCompatActivity() {
         binding.rb1.setOnCheckedChangeListener { _, isChecked ->
             println("teste")
             percentage = 10
-
         }
 
         binding.rb2.setOnCheckedChangeListener { _, isChecked ->
             println("teste")
             percentage = 15
-
         }
-
         binding.rb3.setOnCheckedChangeListener { _, isChecked ->
             println("teste")
             percentage = 20
-
         }
 
         binding.calcular.setOnClickListener {
-
             val ValorTotal = binding.tilTotalConta.text.toString().toFloat()
             val NumPessoas = binding.tilNumPessoas.text.toString().toFloat()
-
             val ValporPessoa = ValorTotal / NumPessoas
             val Gorjeta =  ValporPessoa * (percentage / 100)
             val TotalComGorj = (ValorTotal / NumPessoas) * (percentage / 100)
-
-
 
 
             println(binding.tilNumPessoas)
